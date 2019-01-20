12fabb79a3d375e9a5ae32da625ed0dad42f8439
Chris McLain <christopher@homerunss.com>
2019-01-20 02:14:12 -0600

    Unscramble game implemented

--- src/Unscramble.kt
+++ src/Unscramble.kt
@@ -6,13 +6,38 @@ import kotlin.browser.window
 class Unscramble() {
 
     private var status: Status = Status.IN_PROGRESS
-    private val word = spellmate.wordList.getRandomWord()
+    private var word = spellmate.wordList.getRandomWord()
+    private var shuffled = word.toList().shuffled().joinToString(" ")
+    private var guess = ""
+    private var counter = 0
 
     private val wordField = document.getElementById("unscrambleWord") as HTMLDivElement
-    private val userInput = document.getElementById("unscrambleInput") as HTMLDivElement
+    private val counterField = document.getElementById("unscrambleCounter") as HTMLDivElement
+    private val userInput = document.getElementById("unscrambleInput") as HTMLInputElement
 
     init {
-        wordField.innerHTML = word
+        userInput.addEventListener("input", {
+            guess = userInput.value
+
+            if(guess.equals(word)) {
+                reset()
+            }
+
+            updateVisuals();
+        })
+
+        updateVisuals()
     }
 
+    private fun updateVisuals() {
+        wordField.innerText = shuffled
+        counterField.innerText = "$counter word(s) unscrambled."
+    }
+
+    private fun reset() {
+        userInput.value = ""
+        word = spellmate.wordList.getRandomWord()
+        shuffled = word.toList().shuffled().joinToString(" ")
+        counter++
+    }
 }
